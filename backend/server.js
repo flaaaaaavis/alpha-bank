a1d464504475b100663815a7e556bd88957a4587
Rodrigo <rodrigolfranca@gmail.com>
2022-06-03 13:07:59 -0300

    put and delete

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ app.use(cors( { credentials: true , origin: 'http://localhost:3000'} ));
 //ROUTER
 const ROUTER_POST = require("./routers/routerPost");
 const ROUTER_GET = require("./routers/routerGet");
+const ROUTER_PUT = require("./routers/routerPut");
+const ROUTER_DELETE = require("./routers/routerDelete");
 
-app.use("/", ROUTER_POST, ROUTER_GET);
+app.use("/", ROUTER_POST, ROUTER_GET, ROUTER_DELETE, ROUTER_PUT);
 
 app.listen(port, () => console.log("Servidor Aberto em 4000"));
