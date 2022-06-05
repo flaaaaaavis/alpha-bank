1a37bcb6eea9b48a280feaa8d9bb676d5d48b82f
gskudo <gskudo@yahoo.com.br>
2022-06-05 15:45:32 -0300

    add updat users

--- backend/routers/routerPut.js
+++ backend/routers/routerPut.js
@@ -5,7 +5,21 @@ const jwt = require('jsonwebtoken');
 const { compare } = require('../src/hashController');
 
 router.put('/update/?user', (req, res) => {
-
+    const users = getUsers()
+        users.map(user => {
+                if (user.id === req.params.id){
+                    return {
+                        ...user,
+                        ...req.body
+                    }
+                }
+                return user
+        })
+            // mensagem de sucesso 
+            res.status(200).send('Alteração realizada com sucesso')
 });
 
 module.exports = router;
+
+
+
