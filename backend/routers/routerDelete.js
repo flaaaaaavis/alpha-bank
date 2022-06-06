8cd26f985c3fbe24cb4db68e826ac70f83a17bfc
flavia <sflavia051@gmail.com>
2022-06-06 09:54:23 -0300

    Added first sql script n fixed its bugs

--- backend/routers/routerDelete.js
+++ backend/routers/routerDelete.js
@@ -1,11 +1,106 @@
 const express =  require('express')
 const router = express.Router();
-require("dotenv").config({ path: __dirname+"/../.env"});
+require("dotenv").config({ path: __dirname + "/../.env"});
 const jwt = require('jsonwebtoken');
 const { compare } = require('../src/hashController');
 
-router.delete('/delete/?user', (req, res) => {
+let users = [
+    {
+        "id": 1,
+        "uuid": "d230dab8-854e-4e3b-9c54-77bb2780567a",
+        "CPF": 12345678999,
+        "name": 'John alguem',
+        "bdate": 1980-07-06,
+        "password": "um"
+    },
+    {
+        "id": 2,
+        "uuid": "79087000-068e-4d1a-9ee5-e85ae5e80782",
+        "CPF": 09752564686,
+        "name": 'Mary algo',
+        "bdate": 1990-02-05,
+        "password": "dois"
+    },
+    {
+        "id": 3,
+        "uuid": "690525bc-bdef-474c-a2ba-eabaefe603b3",
+        "CPF": 35647964689,
+        "name": 'Pedro tal',
+        "bdate": 1992-05-01,
+        "password": "tres"
+    },
+    {
+        "id": 1,
+        "uuid": "c5ab6436-8c0d-4576-a4de-0ee76f646a54",
+        "CPF": 364736583658,
+        "name": 'Joana um',
+        "bdate": 1994-03-02,
+        "password": "quatro"
+    },
+    {
+        "id": 5,
+        "uuid": "3cee5b16-2515-4ec2-9a19-ae3d0588efe7",
+        "CPF": 74829583659,
+        "name": 'Andre fulano',
+        "bdate": 1997-03-05,
+        "password": "cinco" 
+    }
+]
 
+router.put('/deleteUser', async (req, res) => {
+    try {
+        const sql = "UPDATE users SET deleted_by = $1, deleted_at = $2 WHERE user_id = $3;";
+        const values = [req.body.deletedBy, req.body.deletedAt, req.body.id];
+        await pool.query(sql, values);
+        console.log("Remoção de usuário bem sucedida!");
+
+        const sqlResult = "SELECT * FROM users WHERE deleted = $1;";
+        const valuesResult = [false];
+        const result = await pool.query(sqlResult, valuesResult);
+
+        res.send(result)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+
+    let users2
+    try {
+        users2 = users.map(user => {
+            if(user.id === req.body.id) {
+                user.deleted_by = req.body.deletedBy;
+                user.deleted_at = req.body.deletedAt;
+                console.log(user);
+                return user;
+            }
+            return user;
+        })
+    } catch (error) {
+        res.status(400).send(error)
+    } finally {
+        users = users2;
+        res.sendStatus(200);
+    }
 });
+
+router.put('/deleteAccount', async (req, res) => {
+    
+})
+
+router.put('/deleteCard', async (req, res) => {
+    
+})
+
+router.put('/deleteTransaction', async (req, res) => {
+    
+})
+
+router.put('/deleteInvoice', async (req, res) => {
+    
+})
+
+router.put('/deletePurchase', async (req, res) => {
+    
+})
 
 module.exports = router;
---
+++ sqlScripts/01.createDataBase.sql
@@ -0,0 +1,118 @@
+CREATE TABLE public.users (
+	"created_by" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_by" integer,
+	"updated_at" TIMESTAMP,
+	"deleted_by" integer,
+	"deleted_at" TIMESTAMP,
+	"user_id" serial NOT NULL,
+	"uuid" varchar(32) NOT NULL UNIQUE,
+	"CPF" varchar(11) NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"bdate" DATE NOT NULL,
+	"password" varchar(50) NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("user_id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.accounts (
+	"created_by" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_by" integer,
+	"updated_at" TIMESTAMP,
+	"deleted_by" integer,
+	"deleted_at" TIMESTAMP,
+	"user_id" serial NOT NULL,
+	"id" serial NOT NULL,
+	"number" serial NOT NULL UNIQUE,
+	"balance" numeric(9,2) NOT NULL,
+	CONSTRAINT "accounts_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.cards (
+	"created_by" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_by" integer,
+	"updated_at" TIMESTAMP,
+	"deleted_by" integer,
+	"deleted_at" TIMESTAMP,
+	"account_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	"id" serial NOT NULL,
+	"number" integer NOT NULL UNIQUE,
+	"expirity_date" DATE NOT NULL,
+	"password" numeric(6) NOT NULL,
+	"SSID" numeric(3) NOT NULL,
+	CONSTRAINT "cards_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.transactions (
+	"created_by" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_by" integer,
+	"updated_at" TIMESTAMP,
+	"deleted_by" integer,
+	"deleted_at" TIMESTAMP,
+	"sender_account" integer NOT NULL,
+	"receiver_account" integer NOT NULL,
+	"id" serial NOT NULL,
+	"value" numeric(9,2) NOT NULL,
+	"date" TIMESTAMP NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.invoices (
+	"created_by" integer NOT NULL,
+	"created_at" TIMESTAMP NOT NULL,
+	"updated_by" integer,
+	"updated_at" TIMESTAMP,
+	"deleted_by" integer,
+	"deleted_at" TIMESTAMP,
+	"id" serial NOT NULL,
+	"card_id" integer NOT NULL,
+	"due_date" TIMESTAMP NOT NULL,
+	"total_value" numeric(9,2) NOT NULL,
+	CONSTRAINT "invoices_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.purchases (
+	"id" serial NOT NULL,
+	"invoice_id" integer NOT NULL,
+	"seller" varchar(20) NOT NULL,
+	"date" TIMESTAMP NOT NULL,
+	"value" numeric(9,2) NOT NULL,
+	CONSTRAINT "purchases_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.sessions (
+	"jwt" varchar(200) NOT NULL,
+	"user_id" serial NOT NULL,
+	CONSTRAINT "sessions_pk" PRIMARY KEY ("jwt")
+) WITH (
+  OIDS=FALSE
+);
+
+
+ALTER TABLE accounts ADD CONSTRAINT "accounts_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
+
+ALTER TABLE cards ADD CONSTRAINT "cards_fk0" FOREIGN KEY ("account_id") REFERENCES "accounts"("id");
+ALTER TABLE cards ADD CONSTRAINT "cards_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
+
+ALTER TABLE transactions ADD CONSTRAINT "transactions_fk0" FOREIGN KEY ("sender_account") REFERENCES "accounts"("number");
+ALTER TABLE transactions ADD CONSTRAINT "transactions_fk1" FOREIGN KEY ("receiver_account") REFERENCES "accounts"("number");
+
+ALTER TABLE invoices ADD CONSTRAINT "invoices_fk0" FOREIGN KEY ("card_id") REFERENCES "cards"("id");
+
+ALTER TABLE purchases ADD CONSTRAINT "purchases_fk0" FOREIGN KEY ("invoice_id") REFERENCES "invoices"("id");
+
+ALTER TABLE sessions ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("user_id");
