f1bf51562ca8dc55d0fd4282badae06de478b228
Rodrigo <rodrigolfranca@gmail.com>
2022-06-08 17:09:24 -0300

    post incoming
    Co-authored-by: Victoria Maria <maviqla@users.noreply.github.com>

--- backend/routers/routerPost.js
+++ backend/routers/routerPost.js
@@ -2,42 +2,132 @@ const EXPRESS = require("express");
 const router = EXPRESS.Router();
 require("dotenv").config({ path: __dirname+"/../.env"});
 const jwt = require('jsonwebtoken');
-const { compare } = require('../src/hashController');
-
-//Classe de Controle de Sessão
-const session = require('../src/sessionController');
+const { compare, hashPwd } = require('../src/hashController');
+const pool = require("../database.js")
+import { v4 as uuidv4 } from 'uuid';
 
 router.post('/login', (req, res) => {    
 
-    const { login, password } = req.body; 
-        
-    const users = JSONReader('users');
-    const user = users.filter(element => element.login === login);
-    
-    if (compare(password, user[0].password)) {
+    const { email, password } = req.body;
+    try {
+        //refatorar pós banco montado: e se o usuário não existir qual vai ser o retorno?
+        const dbUserPassword = await pool.query(`SELECT password 
+                                                 FROM users 
+                                                 WHERE email = ${email} 
+                                                 AND deleted_by=null;`)   
 
-        
+        if (compare(password, dbUserPassword.rows[0].password)) {
 
-        // Gerando Token        
-        const loggedUser = user[0];
-        const userToken = jwt.sign({ loggedUser }, process.env.SECRET);        
+            try {
 
-        // Gerando Sessão
-        session.createSession(userToken, user[0]);
+                // Gerando Token
+                const loggedUser = await pool.query(`SELECT id 
+                                                     FROM users 
+                                                     WHERE email = ${email} 
+                                                     AND deleted_by=null;`)
+                                                     
+                const userToken = jwt.sign({ user_id: loggedUser.rows[0] }, process.env.SECRET);
+                
+                // Gerando Sessão
+                const session = { jwt: userToken, user_id: loggedUser.rows[0] };
 
-        // Gerando Cookie
-        const thirtyDays = 1000 * 60 * 60 * 24 * 30;
-        res.cookie("token", userToken , {maxAge: thirtyDays, path: '/'});
+                await pool.query(`INSERT INTO sessions (jwt, user_id) VALUES (${session.jwt}, ${session.user_id});`)
 
-        // Enviando Resposta
-        res.status(200).json({ auth: true , userToken });
+                // Gerando Cookie
+                const thirtyDays = 1000 * 60 * 60 * 24 * 30;
+                res.cookie("token", userToken , {maxAge: thirtyDays, path: '/'});
+               
+                // Enviando Resposta
+                res.status(200).json({ auth: true , userToken });
 
-    } else {
+            } catch(error) {
 
-        res.status(401).json({ auth: false , message: "Wrong Password"});
+                console.log(error)
+            }        
 
+        } else {
+
+            res.status(401).json({ auth: false , message: "Wrong Password"});
+
+        }
+
+    } catch(error) {
+
+        console.log(error)
     }
 
 });
+
+router.post("/addUser", async (req, res) => {
+
+    const { name, cpf, email, bDate, password } = req.body;
+    const hashedPassword = hashPwd(password);
+
+    //Creating the User
+    try {
+
+        const sql = "INSERT INTO users (name, cpf, email, bdate, password) VALUES ($1, $2, $3, $4, $5);";
+        const values = [name, cpf, email, bDate, hashedPassword];
+        await pool.query(sql, values);
+
+    } catch(error) {
+
+        console.log(error)
+
+    }
+
+    //Creating Account
+    try {
+        
+        const user = await pool.query(`SELECT id 
+                                       FROM users 
+                                       WHERE email = ${email} 
+                                       AND deleted_by=null`);
+        
+        const accountQuery = "INSERT INTO accounts (user_id, uuid, balance) values ($1, $2, $3)"        
+        const accountValues= [user.rows[0], uuidv4(), 0.00]
+
+        
+
+    }
+
+
+    
+    
+})
+
+router.post('/transactions', async (req, res) => {
+    
+    const {method, value} = req.body;
+
+    switch(method){
+
+        case 'cpf':
+            await pool.query (`SELECT cpf, name 
+                               FROM user 
+                               WHERE cpf = ${cpf}, name = ${name}`);
+        break;
+
+        case 'accountCode':
+            await pool.query (`SELECT number, users.name
+                               FROM accounts
+                               INNER JOIN users
+                               ON user_id = users.id
+                               WHERE number = (${value})`)
+        break;
+        
+    }
+
+    if (method == 'cpf') {
+        
+    }    
+
+})
+
+                            // (`SELECT accounts.number, users.name  
+                            //     FROM (users
+                            //     INNER JOIN name ON users.name = ${name}
+                            //     INNER JOIN accountNumber on users.accountName = 
+                            //     )`);
 
 module.exports = router;
